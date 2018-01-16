6a1d13ecc0f7787a901884d9ac25349973a5e7ff
bbubbush <bbubbush@naver.com>
2018-01-16 17:14:38 +0900

    2018-01-16
    2490 풀이 아직

---
+++ BOJ/2490.py
@@ -0,0 +1,6 @@
+result = ['D', 'C', 'B', 'A', 'E']
+for i in range(3):
+    in_data = list(map(int, input().split()))
+    print(result[sum(in_data)])
+   
+# https://github.com/bbubbush/algorithm/blob/master/BOJ/2490.py
