260be66c8f3a0bf18c40fd79132d239d63e3e5a0
이상훈 <bbubbush@gmail.com>
2019-03-23 22:48:13 +0900

    2019-03-22

--- Data_structure/TestTree.java
+++ Data_structure/TestTree.java
@@ -54,29 +54,49 @@ class Tree {
         Node right;
         Node(int data) {
             this.data = data;
-            System.out.println(data);
+
         }
     }
 
     Node root;
 
-    public void makeTree(int[] a) {
-        root = makeTreeR(a, 0, a.length -1);
-
+    // 생성자
+    public Tree() {
+        this(10);
     }
 
+    public Tree(int a) {
+        int[] arr = new int[a];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        makeTree(arr);
+    }
+
+    // 트리 생성
+    public void makeTree(int[] a) {
+        root = makeTreeR(a, 0, a.length -1);
+    }
+
+    // Tree를 밸런스를 맞춰서 생성
     public Node makeTreeR(int[] a, int start, int end) {
-        if (start > end ) return null;
+        if (start > end ) {
+            return null;
+        }
+
         int mid = (start + end ) /2 ;
         Node node = new Node(a[mid]);
         node.left = makeTreeR(a, start, mid -1);
         node.right = makeTreeR(a, mid + 1, end);
-
+        // 관계를 텍스트로 출력
+        System.out.println("data is " + (node != null ? node.data : "None")
+                + ", left child node data is " + (node.left != null ? node.left.data : "None")
+                + ", right child node data is " + (node.right != null ? node.right.data : "None"));
         return node;
     }
 
+    // Tree의 특정 Node를 탐색
     public void searchBTree(Node n, int find) {
-
         if (find < n.data) {
             System.out.println("Data is smaller than " + n.data);
             searchBTree(n.left, find);
@@ -88,17 +108,40 @@ class Tree {
         }
 
     }
+
+    // Check Balance
+    public boolean isBalanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        int heightDiff = getHeight(node.left) - getHeight(node.right);
+        if (Math.abs(heightDiff) > 1) {
+            return false;
+        }
+        else {
+            return isBalanced(node.left) && isBalanced(node.right);
+        }
+    }
+
+    // Node의 깊이를 탐색
+    public int getHeight(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return Math.max(getHeight(node.left), getHeight(node.right));
+    }
 }
 
 public class TestTree {
     public static void main(String[] args) {
-        int[] arr = new int[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        Tree t = new Tree();
-        t.makeTree(arr);
-        t.searchBTree(t.root, 8);
+        // 최소 높이 트리 만들기 & 검색
+        Tree t = new Tree(10);
+        t.searchBTree(t.root, 5);
 
+        System.out.println("\n\n");
+
+        // 밸런스 트리인지 확인하기
+//        Tree t2 = new Tree(10);
+//        System.out.println("Tree balanced is " + t2.isBalanced(t2.root));
     }
 }
